56f90d4f653d27edbbd56c9a53b29286457ff6b5
emmanueltoluwanimi <emmanueltoluwanimi01@gmail.com>
2024-12-18 15:39:08 +0100

    style: responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PropertyListing from "./component/PropertyListing";
 
 export default function Home() {
   return (
-    <main className="p-20">
+    <main className="p-10 md:p-20">
       <div className="text-center mb-10">
         <h1 className="font-bold text-3xl">PropList</h1>
         <p className="text-sm">Find listed properties here.</p>
